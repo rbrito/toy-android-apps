af44bb4409abfade79ab0989886d6102f121dc4e
Rogério Brito <rbrito@ime.usp.br>
2014-02-02 17:37:34 -0200

    primality: Coalesce some ifs.

--- primality/src/com/example/primetest/PrimeTest.java
+++ primality/src/com/example/primetest/PrimeTest.java
@@ -37,11 +37,9 @@ public class PrimeTest extends Activity {
 	}
 
 	public boolean test_prime(long n) {
-		if (n <= 1)
-			return false;
 		if (n == 2 || n == 3)
 			return true;
-		if (n % 2 == 0 || n % 3 == 0)
+		if (n <= 1 || n % 2 == 0 || n % 3 == 0)
 			return false;
 
 		long i = 5;
